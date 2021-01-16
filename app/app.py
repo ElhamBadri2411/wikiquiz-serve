a9755583016238b100966297ead115d7b39bf08d
hadialhakeem <hadihakeem2@gmail.com>
2021-01-16 18:07:45 -0500

    made new endpoint

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from quizhelper import create_questions
 
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +8,14 @@ CORS(app)
 @app.route("/")
 def hello():
     return "Hello World"
+
+@app.route("/generate", methods=['POST'])
+def generate():
+    data = request.get_json(force=True)
+    title = data['title']
+
+    questions = create_questions(title)
+    return jsonify(questions)
 
 
 if __name__ == "__main__":
