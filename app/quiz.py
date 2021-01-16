7684fc84bcfc00fac5c0446fcb8617d9005e4ff0
hadialhakeem <hadihakeem2@gmail.com>
2021-01-16 18:04:07 -0500

    cleaned up quiz.py

--- app/quiz.py
+++ app/quiz.py
@@ -1,13 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-import wikipedia as wiki
 
 app = Flask(__name__)
 CORS(app)
-
-
-print (wiki.summary("donkey", auto_suggest=False))
-print (wiki.page("donkey", auto_suggest=False).content)
 
 @app.route("/")
 def hello():
