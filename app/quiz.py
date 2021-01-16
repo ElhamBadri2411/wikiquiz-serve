5078c0ba85d8722223224aa28fea9fc4ae09d5d2
ElhamBadri2411 <elhambadri24@gmail.com>
2021-01-16 00:05:27 -0500

    your commit message

--- app/quiz.py
+++ app/quiz.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from wikipedia import wiki
 app = Flask(__name__)
+
+def get_info(name):
+    return wiki.summary(name)
+    
 
 @app.route("/")
 def hello():
     return "Hello World"
+
+get_info('whale')
